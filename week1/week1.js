2efb6be0cd23ce57e584beef7deb16446b939255
ssoc2iterpo <iterpo@student.startproject.gr>
2022-11-28 02:34:07 +0200

    week1 -done

---
+++ week1/week1.js
@@ -0,0 +1,63 @@
+// Welcome to programming 
+console.log(2 + 2);
+console.log(1 + 2 + 3 + 4 + 5);
+console.log(20 - 18);
+console.log(2 * 3);
+console.log(8 / 1);
+console.log(3 > 3);
+console.log(2 > 3);
+console.log(10 == 10);
+
+console.log(5 + '5');
+console.log(5 + 10);
+
+
+// Working with conditional statements
+//code 1 
+var result = 30;
+if (result > 40) {
+    console.log('You passed the test')
+}
+else {
+    console.log('You did not pass the test')
+}
+
+// You did not pass the test
+
+//code 2 
+// switch statement 
+var place = 'first';
+switch (place) {
+    case 'first':
+        console.log('first')
+        break;
+    case 'second':
+        console.log('second')
+        break;
+    case 'third':
+        console.log('third')
+        break;
+    default:
+        console.log('default last')
+        break;
+}
+// We should add break !!
+// or it will print all the cases.
+
+for (i = 0; i <= 100; i++) {
+    console.log(i)
+}
+console.log('Go!')
+
+console.log('---------------------')
+i = 0;
+while (i <= 100) {
+    console.log(i)
+    i++;
+}
+
+
+console.log(!false)
+
+
+
