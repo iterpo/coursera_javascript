a94b86a83c9b30bb937d486d582486ae371f9ed9
ssoc2iterpo <iterpo@student.startproject.gr>
2022-12-03 08:44:10 +0200

    week 3 done

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 <a href="https://ssoc2iterpo.github.io/coursera_javascript/week2/week2index.html">Week 2</a> Done 
 <br>
 <a href="https://ssoc2iterpo.github.io/coursera_javascript/week3/week3index.html">Week 3</a>
+Done
 <br>
 <a href="https://ssoc2iterpo.github.io/coursera_javascript/week4/week4index.html">Week 4</a>
 <br>
--- week3/week3c.js
+++ week3/week3c.js
@@ -1,14 +1,77 @@
-const meal = ["soup", "steak", "ice cream"]
-let [starter] = meal;
-console.log(starter);
+// const meal = ["soup", "steak", "ice cream"]
+// let [starter] = meal;
+// console.log(starter);
 
 
-let obj = {
-    key: 1,
-    value: 4
-};
+// let obj = {
+//     key: 1,
+//     value: 4
+// };
 
-let output = { ...obj };
-output.value -= obj.key;
+// let output = { ...obj };
+// output.value -= obj.key;
 
-console.log(output.value);
+// console.log(output.value);
+
+
+// var h1 = document.createElement('h1')
+// h1.innerText = "Type into the input to make this text change"
+
+// var input = document.createElement('input')
+// input.setAttribute('type', 'text')
+
+// document.body.innerText = '';
+// document.body.appendChild(h1);
+// document.body.appendChild(input);
+
+
+var data = {
+    "name": "john",
+    "surname": "terpo",
+    "age": "22",
+    "char": "$"
+}
+
+
+jsonFormat_data = JSON.stringify(data);
+
+console.log(jsonFormat_data)
+console.log(typeof (jsonFormat_data))
+this1 = JSON.parse(jsonFormat_data)
+console.log(this1.surname)
+
+
+class Cake {
+    constructor(lyr) {
+        this.layers = lyr;
+    }
+
+    getLayers() {
+        return this.layers;
+    }
+}
+
+class WeddingCake extends Cake {
+    constructor() {
+        super(2);
+    }
+
+    getLayers() {
+        return super.getLayers() * 5;
+    }
+}
+
+var result = new WeddingCake();
+console.log(result.getLayers());
+
+
+
+
+function scopeTest() {
+    var y = 44;
+
+    console.log(x);
+}
+
+var x = 33;
+scopeTest();
